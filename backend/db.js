b460738eabb406370efa7552d0e3e882ab452e41
Chaitanya Attanti <attanti.chaitanya@iitgn.ac.in>
2026-02-05 15:09:49 +0530

    Add DB_PORT to database connection

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,8 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST || "localhost",
   user: process.env.DB_USER || "root",
   password: process.env.DB_PASSWORD || "Chaitu@2006",
-  database: process.env.DB_NAME || "cur"
+  database: process.env.DB_NAME || "cur",
+  port: process.env.DB_PORT || 3306
 });
 
 db.connect(err => {
